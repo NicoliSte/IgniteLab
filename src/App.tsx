633091d6ea79c2c889b1e0b3e9372d2339e4924e
Nicoli <nickstefan4@gmail.com>
2022-10-14 01:32:52 -0300

    fist commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+export function App() {
+  return (
+    <>
+    <h1 className="font-bold text-2xl text-cyan-500">Hello World</h1>
+
+    <button className="bg-cyan-500 font-medium px-4 oy-2 roudend text-white hover:bg-cyan-300">
+      Enviar
+    </button>
+    </>
+    )
+}
